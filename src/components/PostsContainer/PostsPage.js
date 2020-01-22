a6831ce25338aa6bebc2fbce1fc76ef69db8805e
thisguycodez <guyton578@gmail.com>
2020-01-21 20:27:06 -0500

    took a while to get things showing and the likes to work

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,16 +1,19 @@
-//Complete the necessary code in this file
 import React from "react";
 import Post from "./Post";
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
+const PostsPage = props => {
   return (
     <div className="posts-container-wrapper">
       {/* map through data here */}
+      {props.postData.map(pos => (
+        <div>
+          <Post  post={pos}/>
+        </div>
+      ))}
     </div>
   );
 };
 
 export default PostsPage;
-
